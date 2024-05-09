61eb89ef4de5495502f0dc47fa21bddab134642a
Nishant Banginwar <banginwar.nishant1@gmail.com>
2024-05-09 11:09:52 +0530

    now

--- src/main/java/org/example/productcatalogserviceproxy/Controllers/CategoryController.java
+++ src/main/java/org/example/productcatalogserviceproxy/Controllers/CategoryController.java
@@ -1,4 +1,12 @@
 package org.example.productcatalogserviceproxy.Controllers;
 
+import org.example.productcatalogserviceproxy.Services.ICategoryService;
+
 public class CategoryController {
+
+    ICategoryService categoryService;
+
+    public CategoryController( ICategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 }
